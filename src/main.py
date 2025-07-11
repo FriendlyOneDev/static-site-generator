637aa2cb402625a0bed27ffe72d7ae212f307828
FriendlyOnee <danila.vadimovich@gmail.com>
2025-07-11 13:08:33 +0000

    Webpage generation changed to be recursive

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ def main():
     public_dir = Path(__file__).parent.parent / "public"
     static_dir = Path(__file__).parent.parent / "static"
 
-    markdown_path = Path(__file__).parent.parent / "content/index.md"
+    content_dir = Path(__file__).parent.parent / "content"
     template_path = Path(__file__).parent.parent / "template.html"
 
     if public_dir.exists() and public_dir.is_dir():
@@ -17,8 +17,7 @@ def main():
 
     # shutil.copytree(static_dir, public_dir)
     copy_file_tree(static_dir, public_dir)
-
-    generate_page(markdown_path, template_path, public_dir)
+    generate_pages_recursive(content_dir, template_path, public_dir)
 
 
 def copy_file_tree(src, dst):
@@ -43,13 +42,32 @@ def extract_title(markdown):
         raise Exception("No header provided")
 
 
+def generate_pages_recursive(content_path, template_path, dest_dir_path):
+    for root, dirs, files in os.walk(content_path):
+        relative_path = os.path.relpath(root, content_path)
+        current_dest_dir = os.path.join(dest_dir_path, relative_path)
+
+        os.makedirs(current_dest_dir, exist_ok=True)
+
+        for file in files:
+            if file.endswith(".md"):
+                from_path = os.path.join(root, file)
+                html_filename = (
+                    "index.html"
+                    if file.lower() == "index.md"
+                    else f"{os.path.splitext(file)[0]}.html"
+                )
+                final_dest_path = os.path.join(current_dest_dir, html_filename)
+                generate_page(from_path, template_path, final_dest_path)
+
+
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
-    with open(from_path) as f:
+    with open(from_path, encoding="utf-8") as f:
         markdown = f.read()
 
-    with open(template_path) as f:
+    with open(template_path, encoding="utf-8") as f:
         template = f.read()
 
     html_string = markdown_to_html_node(markdown).to_html()
@@ -58,7 +76,9 @@ def generate_page(from_path, template_path, dest_path):
     html_string = template.replace("{{ Content }}", html_string)
     html_string = html_string.replace("{{ Title }}", title)
 
-    with open(f"{dest_path}/index.html", "w", encoding="utf-8") as f:
+    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+
+    with open(dest_path, "w", encoding="utf-8") as f:
         f.write(html_string)
 
 
